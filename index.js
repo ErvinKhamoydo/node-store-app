59c0dd7617d3be1869efae67e1dd6b8c67f8cf2e
Ervin <hamoydoerik@gmail.com>
2021-01-18 12:32:32 +0200

    Added functionality for editing the product

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
