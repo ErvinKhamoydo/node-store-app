481dc576179f7f3833d29a8bcda264dc4068d9a5
Ervin <hamoydoerik@gmail.com>
2021-01-19 10:26:30 +0200

    Added cart

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const path = require('path');
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const cartRoutes = require('./routes/cart');
 
 const app = express();
 
@@ -19,12 +21,13 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 // add middleware
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
+app.use('/cart', cartRoutes);
 
 const PORT = process.env.PORT || 3000;
 
--- public/app.js
+++ public/app.js
@@ -1,6 +1,44 @@
-document.querySelectorAll('.price').forEach(node => {
-   node.textContent = new Intl.NumberFormat('us-US', {
+const toCurrency = price => {
+   return new Intl.NumberFormat('us-US', {
       currency: 'usd',
       style: 'currency'
-   }).format(node.textContent);
+   }).format(price);
+}
+document.querySelectorAll('.price').forEach(node => {
+   node.textContent = toCurrency(node.textContent);
 });
+
+const $cartItems = document.querySelector('#cart-items');
+
+if ($cartItems) {
+   $cartItems.addEventListener('click', event => {
+      if (event.target.classList.contains('remove-item-from-cart')) {
+         const id = event.target.dataset.id;
+         
+         fetch(`/cart/remove/${id}`, {
+            method: 'delete'
+         })
+            .then(res => res.json())
+            .then(cart => {
+               if (cart.courses.length) {
+                  const html = cart.courses.map(c => {
+                     return `
+                        <tr>
+                           <td>${c.title}</td>
+                           <td>${c.count}</td>
+                           <td>
+                              <button class="btn btn-small remove-item-from-cart" data-id="${c.id}">Delete</button>
+                           </td>
+                        </tr>
+                     `;
+                  }).join('');
+
+                  $cartItems.querySelector('tbody').innerHTML = html;
+                  $cartItems.querySelector('.price').textContent = toCurrency(cart.price);
+               } else {
+                  $cartItems.innerHTML = '<p>Cart is empty</p>';
+               }
+            });
+      }
+   });
+}
