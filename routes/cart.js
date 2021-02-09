46d205686577192b574577542e2b7978deebcf06
Ervin <hamoydoerik@gmail.com>
2021-02-09 10:29:14 +0200

    Finished cart functionality

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,20 @@
 const {Router} = require('express');
 const Course = require('../models/course');
-
 const router = Router();
+
+function mapCartItems(cart) {
+   return cart.items.map(c => ({
+      ...c.courseId._doc,
+      count: c.count,
+      id: c.courseId.id
+   }));
+}
+
+function computePrice(courses) {
+   return courses.reduce((total, course) => {
+      return total += course.price * course.count;
+   }, 0);
+}
 
 router.post('/add', async (req, res) => {
    const course = await Course.findById(req.body.id);
@@ -10,18 +23,32 @@ router.post('/add', async (req, res) => {
 });
 
 router.delete('/remove/:id', async (req, res) => {
-   const cart = await Cart.remove(req.params.id);
+   console.log(req);
+   await req.user.removeFromCart(req.params.id);
+   const user = await req.user
+      .populate('cart.items.courseId')
+      .execPopulate();
+   const courses = mapCartItems(user.cart);
+   const cart = {
+      courses,
+      price: computePrice(courses)
+   };
+   
    res.status(200).json(cart);
 });
 
 router.get('/', async (req, res) => {
-   const cart = await Cart.fetch();
+   const user = await req.user
+      .populate('cart.items.courseId')
+      .execPopulate();
+
+   const courses = mapCartItems(user.cart);
 
    res.render('cart', {
       title: '',
       isCart: true,
-      courses: cart.courses,
-      price: cart.price
+      courses,
+      price: computePrice(courses)
    });
 });
 
