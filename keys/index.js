69dae5949fa9f67930d5006a5d71eea477c302ba
Ervin <hamoydoerik@gmail.com>
2021-02-25 14:35:50 +0200

    Keys protection

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-   MONGODB_URI: `mongodb+srv://ervin_khamoido:HC7pdifd3Lymwc2F@cluster0.ig4sx.mongodb.net/store?retryWrites=true&w=majority`,
-   SESSION_SECRET: 'some secter value',
-   SENDGRID_API_KEY: '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',
-   EMAIL_FROM: 'hamoydoerik@gmail.com',
-   BASE_URL: 'http://localhost:3000'
-};
+if (process.env.NODE_ENV === 'production') {
+   module.exports = require('./keys.prod');
+} else {
+   module.exports = require('./keys.dev');
+}
---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+   MONGODB_URI: `mongodb+srv://ervin_khamoido:HC7pdifd3Lymwc2F@cluster0.ig4sx.mongodb.net/store?retryWrites=true&w=majority`,
+   SESSION_SECRET: 'some secter value',
+   SENDGRID_API_KEY: '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',
+   EMAIL_FROM: 'hamoydoerik@gmail.com',
+   BASE_URL: 'http://localhost:3000'
+};
